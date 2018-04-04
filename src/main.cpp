d10ae3b78ce10aa79ca947098eeb73a994580f97
Caddy Dz <tbitw31@gmail.com>
2018-04-04 14:48:43 +0100

    Display help
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,30 @@ using namespace std;
 int main(int argc, char *argv[]) {
 
 	// Define strings
-	std::string current_exec_name = argv[0]; // Name of the current exec program
-	std::string home_dir = "~/";
+	string current_exec_name = argv[0]; // Name of the current exec program
+	string first_arg = argv[1];
+	string home_dir = "~/";
 
 	if(argv[1] == NULL) {
-		cout << "You must enter a directory name" << '\n';
-		cout << "Enter " + current_exec_name + " --help for instructions" << endl;
+		cout << "Expected arguments, please check the help : " + current_exec_name + " –help" << '\n';
 		return 1;
+	} else if (first_arg == "-help") {
+		cout << "Name: " + current_exec_name << endl;
+		cout << "Description: Create a new preconfigured project initialized with templates to get started faster." << "\n";
+		cout << "Syntax: " + current_exec_name + " name_of_project" << "\n";
+		cout << "args: " << "\n";
+		cout << "name_of_project: (Any valid alpha_dash string literal is accepted)" << " the name of the directory and project to be created." << "\n";
+		cout << "-help: " << "Shows this help menu and instructions and then exit." << "\n";
+		cout << "--Py: " << "Creates a project with preconfigurations tailored for Python projects." << "\n";
+		cout << "--C++: " << "Creates a project with preconfigurations tailored for C++ projects." << "\n";
+		cout << "--C: " << "Creates a project with preconfigurations tailored for C projects." << "\n";
+		cout << "--Latex: " << "Creates a project with preconfigurations tailored for Latex documents projects." << "\n";
+		cout << "--BEAMER: " << "Creates a project with preconfigurations tailored for Beamer projects." << "\n";
+		cout << "Author: " << "Benguergoura Soumeya (bsoumeya)" << endl;
+		return 0;
 	}
 	
-	std::string sPath = argv[1];
+	string sPath = argv[1];
 	mode_t nMode = 0733; // UNIX style permissions
 	int nError = 0;
 	#if defined(_WIN32)
